1a97061c1952000ca81b187ee2c4f09c4376cbbf
akshay481998 <kadamakshays911@gmail.com>
2023-04-25 17:48:11 +0530

    completed data transformation phase

--- sensor/config.py
+++ sensor/config.py
@@ -12,3 +12,5 @@ env_var = EnvironmentVariable()
 
 # Provide the mongodb localhost url to connect python to mongodb.
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
+
+TARGET_COLUMN="class"
